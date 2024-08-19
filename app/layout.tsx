44e7b762c25090edb413454e74e6b36331813c0c
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-08-19 08:52:54 -0700

    feat: chadcn/ui test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Noto_Sans_SC, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
  
 const fontSansCn = Noto_Sans_SC({
   subsets: ["latin"],
@@ -28,15 +28,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh-CN">
+    <>
+    <html lang="zh-CN" >
       <head />
-      <body
-        className={cn(
+      <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSansEn.variable, fontSansCn.variable, fontSansMono.variable
         )}
-      >
+        >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
+  </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,67 @@
-import Image from "next/image";
+"use client"
+ 
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+ 
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+ 
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className={`mb-3 text-2xl font-semibold`}>
-        GW2 简体中文 Mod 官方网页
-        未完成
-      </h2>
-    </main>
-  );
+    <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        花园战争 2 简体中文 Mod 官方网页，测试中。
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        N网：https://www.nexusmods.com/plantsvszombiesgardenwarfare2/mods/379/
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        N网：https://www.nexusmods.com/plantsvszombiesgardenwarfare2/mods/379/
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        ParaTranz：https://paratranz.cn/projects/9739
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        QQ群：796783086，577513205，940656816
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        以下均为测试内容，请勿点击。
+      </p>
+      <Button>Click me</Button>
+      <ModeToggle/>
+    </div>
+  )
 }
