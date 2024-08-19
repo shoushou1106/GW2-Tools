e13ebde52b6ae2e6fcbc770232b71651f35c64c4
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-08-19 08:58:31 -0700

    test: remove ModeToggle

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export function ModeToggle() {
 
 export default function Home() {
   return (
-    <div>
+    <>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         花园战争 2 简体中文 Mod 官方网页，测试中。
       </h1>
@@ -57,11 +57,10 @@ export default function Home() {
       <p className="leading-7 [&:not(:first-child)]:mt-6">
         QQ群：796783086，577513205，940656816
       </p>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <h2 className="leading-7 [&:not(:first-child)]:mt-6">
         以下均为测试内容，请勿点击。
-      </p>
+      </h2>
       <Button>Click me</Button>
-      <ModeToggle/>
-    </div>
+    </>
   )
 }
