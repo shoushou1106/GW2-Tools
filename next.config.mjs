25e2b9c9fdc765a9b4c27ad47afc852ca98eede8
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-08-20 21:58:10 -0700

    feat: revert Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /**
@@ -13,14 +11,19 @@ const nextConfig = {
    * Set base path. This is usually the slug of your repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   
-  basePath: "/",
-*/
+   */
+  basePath: "/gw2-zhCN",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
