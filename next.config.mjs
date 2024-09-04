97e02d5324146e62bf0c7911d8f101972f9a44fa
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-09-03 20:53:38 -0700

    Add root path

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  //basePath: "/gw2-zhCN",
+  basePath: "/zhcn",
 
   /**
    * Disable server-based image optimization. Next.js does not support
