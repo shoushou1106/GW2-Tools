6a64b3e04377e4d2b3a5cba951623aa9fb07f36d
shoushou1106 <100991210+shoushou1106@users.noreply.github.com>
2024-09-03 17:41:03 -0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -2,12 +2,9 @@ import {
   LucideProps,
   Moon,
   SunMedium,
-  Twitter,
   type Icon as LucideIcon,
 } from "lucide-react"
 import Image from "next/image"
-
-export type Icon = LucideIcon
 
 export const Icons = {
   sun: SunMedium,
